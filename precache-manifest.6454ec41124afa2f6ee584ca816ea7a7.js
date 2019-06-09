130d9ef3afe402d00603f4fadfb76a9f8d30cbea
Dylan Casey <tuf94547@temple.edu>
2019-06-08 22:34:32 -0400

    Updates

---
+++ precache-manifest.6454ec41124afa2f6ee584ca816ea7a7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1020223da39ed388925501f210723a57",
+    "url": "/Dylan_Portfolio/index.html"
+  },
+  {
+    "revision": "f221c29245337989828a",
+    "url": "/Dylan_Portfolio/static/css/main.2468be54.chunk.css"
+  },
+  {
+    "revision": "0095239c214cfedab717",
+    "url": "/Dylan_Portfolio/static/js/2.c6513ef4.chunk.js"
+  },
+  {
+    "revision": "f221c29245337989828a",
+    "url": "/Dylan_Portfolio/static/js/main.23defa84.chunk.js"
+  },
+  {
+    "revision": "6e1b9e5218c044b736c8",
+    "url": "/Dylan_Portfolio/static/js/runtime~main.eb51c941.js"
+  },
+  {
+    "revision": "27bf01e5db001a434681f388a09536ee",
+    "url": "/Dylan_Portfolio/static/media/Above_Average_Template.27bf01e5.png"
+  },
+  {
+    "revision": "4a5f27e1ee04dd3effa52738dee407c2",
+    "url": "/Dylan_Portfolio/static/media/Above_Average_Template_2.4a5f27e1.png"
+  },
+  {
+    "revision": "e2e4b1c6d974de5d52dff4065cce2a03",
+    "url": "/Dylan_Portfolio/static/media/TAPP_3.e2e4b1c6.png"
+  },
+  {
+    "revision": "5e5440c73a76700e7d3a07648362c946",
+    "url": "/Dylan_Portfolio/static/media/TAPP_home.5e5440c7.png"
+  },
+  {
+    "revision": "150c1ffa65ee756f6e21e97a7444bc3c",
+    "url": "/Dylan_Portfolio/static/media/TAPP_settings.150c1ffa.png"
+  },
+  {
+    "revision": "b01630a15bd5cd8436d3c1a1e49b47fc",
+    "url": "/Dylan_Portfolio/static/media/dylan_grad_pic.b01630a1.png"
+  },
+  {
+    "revision": "174bac17b3c078034824fbebcd95a20e",
+    "url": "/Dylan_Portfolio/static/media/laptop_coffee_background.174bac17.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dylan_Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/Dylan_Portfolio/precache-manifest.d73ceefc759db1de475262064cfc8d80.js"
+  "/Dylan_Portfolio/precache-manifest.6454ec41124afa2f6ee584ca816ea7a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/Dylan_Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dylan_Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
